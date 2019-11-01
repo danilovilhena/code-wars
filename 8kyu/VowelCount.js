87095a6e3dd507f2f532446a06e9504338f392ff
Danilo Vilhena <54288190+danilovilhena@users.noreply.github.com>
2019-11-01 17:28:19 -0300

    Update VowelCount.js

--- 8kyu/VowelCount.js
+++ 8kyu/VowelCount.js
@@ -1,7 +1,6 @@
 function getCount(str) {
   var vowelsCount = 0;
   
-  // enter your majic here
   for (let i = 0; i< str.length ; i++) {
     let value = str[i];
     if(value=='a'||value=='e'||value=='i'||value=='o'||value=='u'){
