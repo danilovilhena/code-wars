de8fd1d5eb375d04a3d37b6fde76bc29b5af5d6e
Danilo Vilhena <54288190+danilovilhena@users.noreply.github.com>
2019-11-05 16:10:10 -0300

    Create 101Dalmatians.js

---
+++ 8kyu/101Dalmatians.js
@@ -0,0 +1,14 @@
+function howManyDalmatians(number) {
+  if (number<=10){
+    return "Hardly any";
+  }
+  else if (number>10 && number<=50){
+    return "More than a handful!";
+  }
+  else if (number==101){
+    return "101 DALMATIANS!!!";
+  }
+  else {
+    return "Woah that's a lot of dogs!";
+  }
+}
