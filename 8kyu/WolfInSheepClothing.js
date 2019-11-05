a2a8c7a83eb2bebae22c17a0280187257323a266
Danilo Vilhena <54288190+danilovilhena@users.noreply.github.com>
2019-11-05 16:17:38 -0300

    Create WolfInSheepClothing.js

---
+++ 8kyu/WolfInSheepClothing.js
@@ -0,0 +1,10 @@
+function warnTheSheep(queue) {
+  for (let i = 0; i < queue.length; i++){
+    if (queue[queue.length-1]=='wolf'){
+      return "Pls go away and stop eating my sheep";
+    }
+    if (queue[i]=='wolf'){
+      return "Oi! Sheep number " + (queue.length - i-1) +"! You are about to be eaten by a wolf!";
+    }
+  }
+}
